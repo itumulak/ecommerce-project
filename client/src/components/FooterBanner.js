b0564368a34bff1cd14326fc200f2f2d196ee752
Ian Tumulak <edden87@gmail.com>
2024-10-16 14:47:53 +0800

    Added mobile support on the homepage headline and footer components.

--- client/src/components/FooterBanner.js
+++ client/src/components/FooterBanner.js
@@ -5,23 +5,27 @@ import { urlFor } from "../lib/sanityClient";
 
 const FooterBanner = ({data: { discount, largeText1, largeText2, saleTime, smallText, midText, desc, product, buttonText, image }}) => {
   return (
-    <div className="footer-banner-container">
-      <div className="banner-desc">
+    <div className="mt-[120px] py-[100px] px-[40px] lg:h-[400px] text-white w-full bg-[#f02d34] rounded-2xl relative">
+      <img 
+          src={urlFor(image).url()} 
+          alt="headphones" 
+          className="block lg:absolute lg:top-[-25%] lg:left-[25%]" 
+      />  
+      <div className="flex flex-col lg:flex-row justify-between">
         <div className="left">
-          <p>{discount}</p>
-          <h3>{largeText1}</h3>
-          <p>{largeText2}</p>
-          <p>{saleTime}</p>
+          <p className="text-center lg:text-left m-4">{discount}</p>
+          <h3 className="font-black text-center text-[48px] lg:text-left lg:text-[80px]">{largeText1}</h3>
+          <p className="text-center lg:text-left m-4">{largeText2}</p>
+          <p className="text-center lg:text-left m-4">{saleTime}</p>
         </div>
         <div className="right">
-          <p>{smallText}</p>
-          <h3>{midText}</h3>
-          <p>{desc}</p>
+          <p className="text-center lg:text-left text-lg">{smallText}</p>
+          <h3 className="text-center lg:text-left font-extrabold text-[60px]">{midText}</h3>
+          <p className="text-lg">{desc}</p>
           <Link href={`/product/${product}`}>
-            <button type="button">{buttonText}</button>
+            <button type="button" className="text-center w-full lg:w-max lg:text-left p-4 mt-4 bg-[#f2f1f4] text-black rounded-2xl relative">{buttonText}</button>
           </Link>
         </div>
-        <img src={urlFor(image).url()} alt="headphones" className="footer-banner-image" />
       </div>
     </div>
   )
--- client/src/components/HeroBanner.js
+++ client/src/components/HeroBanner.js
@@ -4,20 +4,30 @@ import React from 'react'
 
 const HeroBanner = ({data: {smallText, midText, largeText, buttonText, image, desc, product}}) => {
   return (
-    <div className="hero-banner-container">
+    <div className="py-24 px-12 bg-[#dcdcdc] rounded-2xl relative">
+      <img 
+        src={urlFor(image).url()} 
+        alt="headphones" 
+        className="lg:absolute lg:w-[450px] lg:h-[450px] lg:top-0 lg:right-[20%]" 
+      />
+
       <div>
-        <p className="beats-solo">{smallText}</p>
-        <h3>{midText}</h3>
+        <p className="text-lg text-center lg:text-left">{smallText}</p>
+        <h3 className="text-4xl lg:text-7xl">{midText}</h3>
         <h1>{largeText}</h1>
-        <img src={urlFor(image).url()} alt="headphones" className="hero-banner-image" />
 
         <div>
           <Link href={`/product/${product}`}>
-            <button type="button">BUTTON TEXT</button>
+            <button 
+              type="button"
+              className="w-full lg:w-max rounded-2xl py-3 px-4 bg-[#f02d34] text-white mt-10 text-lg font-semibold cursor-pointer z-10"
+            >
+              BUTTON TEXT
+            </button>
           </Link>
-          <div className="desc">
-            <h5>Description</h5>
-            <p>{desc}</p>
+          <div className="mt-8 lg:mt-0 lg:absolute lg:right-[5%] lg:bottom-[5%] lg:w-[300px] flex flex-col text-[#324d67]">
+            <h5 className="mb-3 text-lg self-end font-bold">Description</h5>
+            <p className="color-[#5f5f5f] font-light text-end">{desc}</p>
           </div>
         </div>
       </div>
